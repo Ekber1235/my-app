a88190525e5d7d33859dccf9c7c0674ccdb52987
Ekber1235 <ekber160309@gmail.com>
2024-11-12 21:35:48 +0400

    home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css'
+import { useState, useEffect } from 'react';
+function App(){
+  const [item, setItem] = useState({
+    itemName: "",
+    itemPrice: "",
+    itemImg: ""
+  })
+  const [allItems,setAllItems] = useState([])
+  const [searchInp, setSearchInp] = useState("")
+  const [priceInp, setPriceInp] = useState("")
+  const takeItem = (e) =>{
+    setItem({...item, [e.target.name]: e.target.value})
+  }
+
+  const showItem = () =>{
+    setAllItems([...allItems, item])
+  }
+  
+  const orderItem = () =>{
+    setAllItems([...allItems.sort((a,b) => a.itemPrice - b.itemPrice)])
+    console.log(allItems);
+  }
+
+  const searchItem = (e) =>{
+    setSearchInp(e.target.value)
+  }
+
+  const searchPrice = (e) =>{
+    setPriceInp(e.target.value)
+  }
+  
+  const filteredItems = allItems.filter(item => 
+    (searchInp === "" || item.itemName.toLowerCase().includes(searchInp.toLowerCase())) &&
+    (priceInp === "" || item.itemPrice.includes(priceInp))
   );
+  
+  
+      return(
+   <div>
+    <input onChange={takeItem} type='text' placeholder='item name' name='itemName' />
+    <input onChange={takeItem} type='text' placeholder='item price' name='itemPrice' />
+    <input onChange={takeItem} type='text' placeholder='item image' name='itemImg' />
+      <button onClick={showItem}>Show items</button>
+      <button onClick={orderItem}>Order items</button>
+      <input onChange={searchItem} type='text' placeholder='search item' />
+      <input onChange={searchPrice} type='text' placeholder='search price' />
+
+      <br/>
+      <div className='cards'>
+        {
+          allItems.length == 0 ? null :
+          filteredItems.length > 0 ?
+          filteredItems.map((oneItem, i )=> {
+            return (
+             <div className='card' key={i}>
+               <img src={oneItem.itemImg} alt=''/>
+               <h1>{oneItem.itemName}</h1>
+               <h4>{oneItem.itemPrice}</h4>
+             </div>
+            )
+          }) : "hec bir melumat tapilmadi....."
+        }
+      </div>
+
+   </div>
+  
+  )
 }
 
-export default App;
+export default App
+  
+
+
+
